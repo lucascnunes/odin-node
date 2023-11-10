28fc82f505ef22a4fa5ebafa13945088d7be0706
Lucas Nunes <2693803+lucascnunes@users.noreply.github.com>
2023-11-10 20:37:56 -0300

    this one gave me a hard time because I was trying to use slice... had to look the solution 🤦‍♂️

---
+++ 04-removeFromArray/removeFromArray.js
@@ -0,0 +1,14 @@
+const removeFromArray = function (array, ...args) {
+    let newArray = [];
+
+    array.forEach((i) => {
+        if (!args.includes(i)) {
+            newArray.push(i);
+        }
+    });
+
+    return newArray;
+};
+
+// Do not edit below this line
+module.exports = removeFromArray;
