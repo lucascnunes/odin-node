bad00aa0324ab6af30269721d3ff67fb5bdfb928
Lucas Nunes <2693803+lucascnunes@users.noreply.github.com>
2023-11-10 19:40:01 -0300

    starting 02-repeatString

---
+++ 02-repeatString/repeatString.js
@@ -0,0 +1,10 @@
+const repeatString = function (string, repeatTimes) {
+    let repeatedString = "";
+    for (let i = 1; i <= repeatTimes; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
